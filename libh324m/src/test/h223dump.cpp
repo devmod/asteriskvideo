bfbdea74522c683391bc0b7f2ccf06ff0b53e353
sip <sip@57fa1218-b025-0410-bec8-92b503356f72>
2007-01-25 10:46:29 +0000

    git-svn-id: https://asteriskvideo.svn.sourceforge.net/svnroot/asteriskvideo@13 57fa1218-b025-0410-bec8-92b503356f72

---
+++ libh324m/src/test/h223dump.cpp
@@ -0,0 +1,70 @@
+#include "../H223Demuxer.h"
+#include "../H324CCSRLayer.h"
+
+class DumpChannel :
+	public H324CCSRLayer
+{
+public:
+	//Method overrides from ccsrl
+	virtual int OnControlPDU(H324ControlPDU &pdu)
+	{
+		cout << "Received \n" << pdu << "\n";
+		return 1;
+	}
+};
+
+
+int main(int argc,char **argv)
+{
+	// Check number of parameters
+	if (argc<2)
+	{
+		printf("usage: h223dump <filename>\n");
+		return 1;
+	}
+
+	//Open file
+	int f = open(argv[1],O_RDONLY);
+
+	//Check
+	if (f==-1)
+	{
+		printf("unable to open [%s]\n",argv[1]);
+		return 2;
+	}
+
+	H223MuxTable localTable;
+	DumpChannel dumpChannel;
+	H223Demuxer demuxer;
+	
+	//Set entry
+	localTable.SetEntry(0,"","0");
+	
+	//Set demuxer channels
+	demuxer.SetChannel(0,&dumpChannel);
+
+	//Open demuxer
+	demuxer.Open(&localTable);
+
+	//Read
+	unsigned char buffer[160];
+	int len;
+
+	//Until end of file
+	while ((len=read(f,buffer,160))>0)
+	{
+		//For each byte
+		for (int i=0;i<len;i++)
+			 //Demux
+			 demuxer.Demultiplex(buffer[i]);
+	}
+
+	//Close demuxer
+	demuxer.Close();
+
+	//Close file
+	close(f);
+
+	return 0;
+	
+}
---
+++ libh324m/src/test/reverse.cpp
@@ -0,0 +1,59 @@
+extern "C" {
+	#include "../bits.h"
+}
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int main(int argc, char** argv) 
+{
+	//Check params
+	if (argc<3)
+	{
+		printf("usage: reverse <input-file> <output-file>\n");
+		return 1;
+	}
+
+	//Open output
+	int fdOut = open(argv[2],O_CREAT|O_WRONLY);
+
+	//If not opened
+	if (fdOut==-1)
+	{
+		printf("unable to create [%s]\n",argv[2]);
+		return 2;
+	}
+
+	//Open input
+	int fdIn = open(argv[1],O_RDONLY);
+
+	//If not opened
+	if (fdIn==-1)
+	{
+		printf("unable to open [%s]\n",argv[1]);
+		return 2;
+	}
+
+	//Read
+	unsigned char buffer[1024];
+	int len;
+
+	//Until end of file
+	while((len=read(fdIn,buffer,1024))>0)
+	{
+		//Reverse bits
+               	TIFFReverseBits(buffer,len);
+		//Write
+		write(fdOut,buffer,len);	
+	}	
+	
+	//close files
+	close(fdOut);
+	close(fdIn);
+
+	//Exit
+	return(0);
+} 
