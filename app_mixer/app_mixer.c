270b33b609308c1cb5f6a09240e596c34877cfad
sip <sip@57fa1218-b025-0410-bec8-92b503356f72>
2007-06-24 20:32:34 +0000

    It work's!! Press # during call to change composition type.
    git-svn-id: https://asteriskvideo.svn.sourceforge.net/svnroot/asteriskvideo@112 57fa1218-b025-0410-bec8-92b503356f72

--- app_mixer/app_mixer.c
+++ app_mixer/app_mixer.c
@@ -40,79 +40,171 @@
 #include "asterisk/module.h"
 #include "asterisk/rtp.h"
 
-#include <xmlrpcproxyclient.h>
+#include <mcu/xmlrpcmcuclient.h>
 
 static char *app = "mixer_join";
 static char *synopsis = "Add participant to an existing mixing conference";
 static char *descrip = "  mixer_join(confId): Add participant to conference\n";
 
+static struct sched_context *sched;     /*!< The scheduling context */
+static struct io_context *io;           /*!< The IO context */
+
 void *mcu;
 int room;
 
-
 static int join_exec(struct ast_channel *chan, void *data)
 {
-	int res = -1;
 	int format;
 	int partId;
 	int audioPort;
 	int videoPort;
+	int remoteAudioPort;
+	int remoteVideoPort;
 	struct ast_module_user *u;
 	struct ast_rtp *audio;
 	struct ast_rtp *video;
-	struct sockaddr_in *addr;
+	struct sockaddr_in addr;
+	int infds[2];
+	int outfd;
+	int ms;
+	int comp=0;
 
 	u = ast_module_user_add(chan);
+
+	/* log */
+	ast_log(LOG_WARNING,">mixer_joini [%x]\n",chan);
 
 	format = ast_best_codec(chan->nativeformats);
 	ast_set_write_format(chan, format);
 	ast_set_read_format(chan, format);
 
+	/* log */
+	ast_log(LOG_WARNING,"Adding participant [%x]\n",chan);
+
 	/* Create participant */
 	partId = CreateParticipant(mcu,room);
-
-	/* Set codecs */
-
+	ast_log(LOG_WARNING,"created [%x]\n",chan);
 
 	/* Create audio and video rtp sockets */
-	audio = ast_rtp_new(NULL,NULL,0,0);
-	video = ast_rtp_new(NULL,NULL,0,0);
+	audio = ast_rtp_new(sched,io,0,0);
+	video = ast_rtp_new(sched,io,0,0);
+	ast_log(LOG_WARNING,"new [%x]\n",chan);
 
 	/* Get local port address */
-	ast_rtp_get_us(audio,addr);
-	audioPort = ntohs(addr->sin_port);
-	ast_rtp_get_us(video,addr);
-	videoPort = ntohs(addr->sin_port);
+	ast_rtp_get_us(audio,&addr);
+	audioPort = ntohs(addr.sin_port);
+	ast_rtp_get_us(video,&addr);
+	videoPort = ntohs(addr.sin_port);
+	ast_log(LOG_WARNING,"set [%x]\n",chan);
 
-	/* Start sending */
+	/* Set codecs */
+	SetVideoCodec(mcu,room,partId,H263,CIF,300,5);
+	SetAudioCodec(mcu,room,partId,PCMU);
 
-	/* Start receiving */
+	/* Start sending in mcu side*/
+	StartSendingAudio(mcu,room,partId,"127.0.0.1",audioPort);
+	StartSendingVideo(mcu,room,partId,"127.0.0.1",videoPort);
 
-	while (ast_waitfor(chan, -1) > -1) {
-		struct ast_frame *f = ast_read(chan);
-		if (!f)
-			break;
-		f->delivery.tv_sec = 0;
-		f->delivery.tv_usec = 0;
-		switch (f->frametype) {
-		case AST_FRAME_DTMF:
-			if (f->subclass == '#') {
-				res = 0;
-				ast_frfree(f);
+	/* Start receiving in mcu side*/
+	StartReceivingAudio(mcu,room,partId,&remoteAudioPort);
+	StartReceivingVideo(mcu,room,partId,&remoteVideoPort);
+	ast_log(LOG_WARNING,"Loop [%x]\n",chan);
+
+	/* Set send data */
+	addr.sin_family      = AF_INET;
+	addr.sin_addr.s_addr = INADDR_ANY;
+	addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+	/* Set peer addres of local rtp */
+	addr.sin_port = htons(remoteAudioPort);
+	ast_rtp_set_peer(audio,&addr);
+	addr.sin_port = htons(remoteVideoPort);
+	ast_rtp_set_peer(video,&addr);
+
+	/* Fill fd array */
+	infds[0] = ast_rtp_fd(audio);
+	infds[1] = ast_rtp_fd(video);
+
+	/* log */
+	ast_log(LOG_WARNING,"Loop [%x]\n",chan);
+
+	/* Loop */
+	while (1)
+	{
+		/* No output */
+		outfd = -1;
+		/* 10 seconds timeout */
+		ms = 10000;
+		/* Read from channels and fd*/
+		if(ast_waitfor_nandfds(&chan,1,infds,2,NULL,&outfd,&ms))
+		{
+			/* Got data in channel */
+			struct ast_frame *f = ast_read(chan);
+			/* Check for hangup */
+			if (!f)
+				/* Exit */
 				goto end;
+			/* Send it now */
+			f->delivery.tv_sec = 0;
+			f->delivery.tv_usec = 0;
+			/* Depending on type*/
+			switch (f->frametype)
+			{
+				case AST_FRAME_VOICE:
+					/* Send to mixer */
+					ast_rtp_write(audio,f);
+					break;
+				case AST_FRAME_VIDEO:
+					/* Send to mixer */
+					ast_rtp_write(video,f);
+					break;
+				case AST_FRAME_DTMF:
+					/* Get dtmf */
+					if (f->subclass == '#') {
+						/* Loop composition tipe */
+						if (comp==2)
+							comp = 0;
+						else
+							comp++;
+						/* Set it*/
+						SetCompositionType(mcu,room,comp,CIF);	
+					}
+					/* Free frame */
+					ast_frfree(f);
+					break;
+				default:
+					/* Free frame */
+					ast_frfree(f);
 			}
-			/* fall through */
-		default:
-			if (ast_write(chan, f)) {
-				ast_frfree(f);
-				goto end;
-			}
+		} else if (outfd==infds[0]) {
+			/* Read audio */
+			struct ast_frame *f = ast_rtp_read(audio);
+			/* Send it now */
+			f->delivery.tv_sec = 0;
+			f->delivery.tv_usec = 0;
+			/* send to channel */
+			ast_write(chan,f);
+		} else if (outfd==infds[1]) {
+			/* Read video */
+			struct ast_frame *f = ast_rtp_read(video);
+			/* Send it now */
+			f->delivery.tv_sec = 0;
+			f->delivery.tv_usec = 0;
+			/* send to channel */
+			ast_write(chan,f);
 		}
-		ast_frfree(f);
 	}
 end:
+	/* log */
+	ast_log(LOG_WARNING,"Removing participant\n");
+	/* Remove Participant */
+	DeleteParticipant(mcu,room,partId);
+	/* log */
+	ast_log(LOG_WARNING,"<mixer_join\n");
+
+	/* Exit */
 	ast_module_user_remove(u);
-	return res;
+	return -1;
 }
 
 static int unload_module(void)
@@ -129,13 +221,28 @@ static int unload_module(void)
 	/* Destroy MCU proxy */
 	DestroyMCUClient(mcu);
 
+	sched_context_destroy(sched);
+	io_context_destroy(io);
+
 	return res;
 }
 
 static int load_module(void)
 {
+
+	if (!(sched = sched_context_create())) {
+		ast_log(LOG_ERROR, "Unable to create scheduler context\n");
+		return 	AST_MODULE_LOAD_FAILURE;
+	}
+
+	if (!(io = io_context_create())) {
+		ast_log(LOG_ERROR, "Unable to create I/O context\n");
+		sched_context_destroy(sched);
+		return AST_MODULE_LOAD_FAILURE;
+	}
+
 	/* Create MCU proxy */
-	mcu = CreateMCUClient("http://127.0.0.1:8080/");
+	mcu = CreateMCUClient("http://127.0.0.1:8080/mcu");
 
 	/* Create default room */
 	room = CreateConference(mcu,"Default Room");
